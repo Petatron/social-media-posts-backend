3fd55c0d0d87cf3c5ffc328c130393489d687c26
Yibo Fu <yibomus@gmail.com>
2024-06-20 23:39:18 -0400

    fix the UserSerializer fields issue

--- core/user/serializers.py
+++ core/user/serializers.py
@@ -8,6 +8,8 @@ class UserSerializer(serializers.ModelSerializer):
     id = serializers.UUIDField(source='public_id', read_only=True, format='hex')
     created = serializers.DateTimeField(read_only=True)
     updated = serializers.DateTimeField(read_only=True)
+    bio = serializers.CharField(allow_blank=True, allow_null=True, required=False)
+    avatar = serializers.ImageField(allow_null=True, required=False)
 
     class Meta:
         model = User
