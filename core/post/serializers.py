dc3290ef236123f679c21ed37ce00493c3653f8f
ArthLeu <haoyanl2@uci.edu>
2024-07-09 19:50:48 -0700

    added method to verify if the post author is the user authorized

--- core/post/serializers.py
+++ core/post/serializers.py
@@ -9,5 +9,12 @@ class PostSerializer(AbstractSerializer):
         model = Post
         # List of all the fields that can be included in a request or a response
         fields = ['post_id']
-        # # List of all the fields that can only be read by the user
-        read_only_field = ['post_id', 'author']
+        # List of all the fields that can only be read by the user
+        read_only_fields = ['post_id', 'author']
+
+        def is_authorized_user(self, user: str):
+            '''
+            param user: the user authorized to perform the action, used to compared with the post author
+            '''
+            # logic here to check if the post author is the authorized user
+            return self.model.author == user
