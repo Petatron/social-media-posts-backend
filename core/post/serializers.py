fafaa161985e3cf5c46448c0d298bb91fe9dd57b
Yibo Fu <yibomus@gmail.com>
2024-07-11 14:08:09 -0400

    update author validation method

--- core/post/serializers.py
+++ core/post/serializers.py
@@ -1,20 +1,19 @@
 from core.abstract.serializer import AbstractSerializer
 from core.post.models import Post
 
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
 
 class PostSerializer(AbstractSerializer):
-    # Rewriting some fields like the public id to be represented as the id of the object
+    author = serializers.SlugRelatedField(queryset=Post.objects.all(), slug_field='public_id')
+
+    def validate_author(self, value):
+        if self.context["request"].user != value:
+            raise ValidationError("You can't create a post for another user.")
+        return value
 
     class Meta:
         model = Post
-        # List of all the fields that can be included in a request or a response
-        fields = ['post_id']
         # List of all the fields that can only be read by the user
-        read_only_fields = ['post_id', 'author']
-
-        def is_authorized_user(self, user: str):
-            '''
-            param user: the user authorized to perform the action, used to compared with the post author
-            '''
-            # logic here to check if the post author is the authorized user
-            return self.model.author == user
+        read_only_fields = ['edited']
