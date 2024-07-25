1b1a510cca1f1aac00f0396f26dcb56cab317105
Yibo Fu <yibomus@gmail.com>
2024-07-24 23:56:38 -0400

    implement POST viewsets for post api

--- core/post/serializers.py
+++ core/post/serializers.py
@@ -1,12 +1,13 @@
 from core.abstract.serializer import AbstractSerializer
 from core.post.models import Post
+from core.user.models import User
 
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
 
 class PostSerializer(AbstractSerializer):
-    author = serializers.SlugRelatedField(queryset=Post.objects.all(), slug_field='public_id')
+    author = serializers.SlugRelatedField(queryset=User.objects.all(), slug_field='public_id')
 
     def validate_author(self, value):
         if self.context["request"].user != value:
@@ -16,4 +17,5 @@ class PostSerializer(AbstractSerializer):
     class Meta:
         model = Post
         # List of all the fields that can only be read by the user
+        fields = ['id', 'author', 'body', 'edited', 'created', 'updated']
         read_only_fields = ['edited']
