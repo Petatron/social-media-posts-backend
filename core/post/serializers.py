98267cfab685f68cb4a578610e6686c8f997e6e9
Yibo Fu <yibomus@gmail.com>
2024-07-30 21:50:44 -0400

    reslove conflict

--- core/post/serializers.py
+++ core/post/serializers.py
@@ -1,5 +1,6 @@
 from core.abstract.serializer import AbstractSerializer
 from core.post.models import Post
+from core.user.models import User
 
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
@@ -7,7 +8,7 @@ from rest_framework.exceptions import ValidationError
 
 class PostSerializer(AbstractSerializer):
     """Serializer for the Post model."""
-    author = serializers.SlugRelatedField(queryset=Post.objects.all(), slug_field='public_id')
+    author = serializers.SlugRelatedField(queryset=User.objects.all(), slug_field='public_id')
 
     def validate_author(self, value):
         if self.context["request"].user != value:
