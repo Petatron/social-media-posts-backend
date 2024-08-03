020d2bba652ebe0179cf74b76d211e0774e8ea76
ArthLeu <haoyanl2@uci.edu>
2024-08-03 14:19:04 -0700

    PETA-117 added to_representation

--- core/post/serializers.py
+++ core/post/serializers.py
@@ -8,12 +8,18 @@ from rest_framework.exceptions import ValidationError
 
 class PostSerializer(AbstractSerializer):
     """Serializer for the Post model."""
-    author = serializers.SlugRelatedField(queryset=User.objects.all(), slug_field='public_id')
+    author = serializers.SlugRelatedField(queryset=User.objects.all(), slug_field='username')
 
     def validate_author(self, value):
         if self.context["request"].user != value:
             raise ValidationError("You can't create a post for another user.")
         return value
+
+    def to_representation(self, instance):
+        """Convert the instance to a dictionary."""
+        data = super().to_representation(instance)
+        data["author_uid"] = instance.author.public_id
+        return data
 
     class Meta:
         model = Post
